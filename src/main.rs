af5bf3a78c8e88881c87c5f88089b6bd204b7c64
Marshmallo <193443004+iwpurz@users.noreply.github.com>
2026-09-15 20:55:23 -0500

    Refactor ZIP utility function calls to use ZipUtils

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{
     path::{Path, PathBuf},
     sync::OnceLock,
 };
-use zip_extensions::{zip_create_from_directory, zip_extract};
+use zip_extensions::ZipUtils;
 
 const PROXIES_URL: &str =
     "https://raw.githubusercontent.com/Stefanuk12/RoProPatcher/master/proxies.txt";
@@ -133,7 +133,7 @@ async fn download_patch(selected_proxy: &str) -> Result<()> {
     }
 
     let mut cursor = Cursor::new(extension_bytes);
-    zip_extract(&mut cursor, &extract_dir).context("Failed to extract ZIP archive")?;
+    ZipUtils::extract(&mut cursor, &extract_dir).context("Failed to extract ZIP archive")?;
 
     patch_extension_dir(&extract_dir, selected_proxy)?;
     println!("Finished downloading and patching.");
@@ -169,7 +169,7 @@ async fn main() -> Result<()> {
         let source_dir = PathBuf::from("RoPro");
         let zip_out = PathBuf::from("RoPro-PATCHED.zip");
 
-        zip_create_from_directory(&zip_out, &source_dir)
+        ZipUtils::create_from_directory(&zip_out, &source_dir)
             .context("Unable to create output ZIP archive")?;
 
         fs::remove_dir_all(source_dir).context("Unable to clean up RoPro directory")?;
